a63590dea757e80b450659bfc470a9de6dcf86e9
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2024-07-15 13:05:08 +0300

    Add test data

---
+++ ui/apos/components/AposTranslationMeta.vue
@@ -0,0 +1,20 @@
+<template>
+  <AposTranslationIndicator
+    v-if="isVisible"
+    label="aposAutomaticTranslation:fieldMeta"
+  />
+</template>
+
+<script>
+import AposFieldMetaUtilsMixin from 'Modules/@apostrophecms/doc/mixins/AposFieldMetaUtilsMixin';
+
+export default {
+  name: 'AposTranslationMeta',
+  mixins: [ AposFieldMetaUtilsMixin ],
+  data() {
+    return {
+      isVisible: this.meta.data?.changed
+    };
+  }
+};
+</script>
